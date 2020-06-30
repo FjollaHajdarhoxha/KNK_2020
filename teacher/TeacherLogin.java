bb08c03ccfea08c5bb4d5ba03cc5da6a4401cc2d
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-06-30 19:44:32 +0200

    Update TeacherLogin.java

--- teacher/TeacherLogin.java
+++ teacher/TeacherLogin.java
@@ -11,6 +11,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+
 import javafx.scene.input.KeyCode;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
