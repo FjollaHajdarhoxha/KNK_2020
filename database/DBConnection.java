b4f7917cc0b403cda5c0f6c039e3e264aa1a905c
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2020-06-30 19:42:00 +0200

    Update DBConnection.java

--- database/DBConnection.java
+++ database/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 	
 	private static Connection dbConnection;
 	
-	private final static String host = System.getenv("DB_HOST");
+	private final static String host = System.getenv("DB_HOST"); //emri i hostit ne pc,localhost
 	private final static String dbName = System.getenv("DB_NAME");
 	private final static String username = System.getenv("DB_USERNAME");
 	private final static String password = System.getenv("DB_PASSWORD");
